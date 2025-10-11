ef16918ae8e97274baf4d82ef40590e3c600cef6
Ziyang Li <liby99@icloud.com>
2025-10-11 11:17:25 -0400

    Updating package submission script

--- package_submission.sh
+++ package_submission.sh
@@ -2,4 +2,4 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 rm -f submission.zip
-zip -r submission.zip targets/ waa/ setup.py requirements.txt
+zip -r submission.zip targets/ waa/ setup.py requirements.txt -x "*__pycache__*" -x "*.DS_Store" -x "*.pyc"
