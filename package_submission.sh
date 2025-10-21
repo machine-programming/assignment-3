d074ce0687f638abcde516d454f498c83d1c5b93
Ziyang Li <liby99@icloud.com>
2025-10-21 16:03:48 -0400

    Do not include node_modules

--- package_submission.sh
+++ package_submission.sh
@@ -2,4 +2,4 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 rm -f submission.zip
-zip -r submission.zip targets/ waa/ setup.py requirements.txt -x "*__pycache__*" -x "*.DS_Store" -x "*.pyc"
+zip -r submission.zip targets/ waa/ setup.py requirements.txt -x "*__pycache__*" -x "*.DS_Store" -x "*.pyc" -x "*node_modules*"
