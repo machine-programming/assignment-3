04ebe77c56ea1db2180666bc2e3e883555e7add9
Ziyang Li <liby99@icloud.com>
2025-10-22 21:33:45 -0400

    Reorder zip command arguments in package_submission.sh

--- package_submission.sh
+++ package_submission.sh
@@ -2,4 +2,4 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 rm -f submission.zip
-zip -r submission.zip targets/ waa/ setup.py requirements.txt -x "*__pycache__*" -x "*.DS_Store" -x "*.pyc" -x "*node_modules*"
+zip -r submission.zip waa/ targets/ setup.py requirements.txt -x "*__pycache__*" -x "*.DS_Store" -x "*.pyc" -x "*node_modules*"
